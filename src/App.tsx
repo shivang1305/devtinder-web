c30d0f4aca88f278691434f1c975563db51903da
Shivang Sharma <sharmashivang1999@gmail.com>
2026-04-06 18:06:21 +0530

    feat: email-verification page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./pages/Home";
+import { EmailVerification } from "./pages/EmailVerification";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Route>
+      <Route path="/verify-email" element={<EmailVerification />} />
     </Routes>
   );
 }
